ead8c132936373c44096531d38ad6f4912ae1ce6
IFamoxI <147991877+IFamoxI@users.noreply.github.com>
2023-12-05 21:18:45 +0600

    Add files via upload

---
+++ Pr4_Base.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+H=11.5  #meters
+S=1.8  #meters^2
+V0=10000 #meters^3/hour
+C0=0.155 #moll/liter
+e=0.05
+Cexp_list=[0, 0.0025, 0.01, 0.026, 0.049, 0.072, 0.09, 0.115, 0.13, 0.146, 0.154]
+Ci_list=[0]
+Ciall_list=[0]
+
+Vr=H*S
+t_av=V0/Vr
+n=2
+
+for n in range(n):
+
+    n+=1
+
+    for time in range(11):
+        Ci=0
+
+print("τ")
---
+++ Pr5_Var_11.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import math
+
+V1 = 2.3*10**-4
+V2 = 5.1*10**-4
+p1 = 890
+p2 = 1030
+C1 = 3000
+C2 = 4240
+d = 0.02
+K = 800
+h = 0.01
+b1 = (K * math.pi * d) / (V1 * p1 * C1)
+b2 = (K * math.pi * d) / (V2 * p2 * C2)
+T1 = [190]
+T2 = [40]
+
+while T1[-1] - T2[-1] > 2:
+    T01 = T1[-1] - h * b1 * (T1[-1] - T2[-1])
+    T1.append(T01)
+    T02 = T2[-1] + h * b2 * (T1[-1] - T2[-1])
+    T2.append(T02)
+
+plt.figure()
+plt.subplot(1, 2, 1)
+x = range(len(T1))
+plt.title("прямоточный теплообменник")
+plt.xlabel("L")
+plt.ylabel("Т")
+plt.plot(x, T1, label="T1")
+plt.plot(x, T2, label="T2")
+plt.legend()
+
+T11 = [200]
+T22 = [T2[-1]]
+
+while T22[-1] > 35:
+    T011 = T11[-1] - h * b1  *(T11[-1] - T22[-1])
+    T11.append(T011)
+    T022 = T22[-1] - h * b2 * (T11[-1] - T22[-1])
+    T22.append(T022)
+    
+plt.subplot(1, 2, 2)
+X = range(len(T11))
+plt.title("противоточный теплообменник")
+plt.xlabel("L")
+plt.ylabel("Т")
+plt.plot(X, T11, label="T1")
+plt.plot(X, T22, label="T2")
+plt.legend()
+plt.show()
+print(T11[-1], T22[0])
